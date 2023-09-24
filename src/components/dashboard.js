fc579fa50a6bbedebe09a596fab15ae9236febef
Yashank <yashank192002@gmail.com>
2023-09-25 02:23:24 +0530

    third

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -17,7 +17,7 @@ const Dashboard = () => {
   useEffect(()=>{
     const fetchData = async () => {
         try {
-          const response = await fetch(`http://localhost:5000/`);
+          const response = await fetch(`https://dashboard-backend-hp9q.onrender.com`);
           if (!response.ok) {
             throw new Error('Network response was not ok');
           }
